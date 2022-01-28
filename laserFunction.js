cf823ea2c2bbec0e6032747cc0934361621e71b1
Rupesh-Darimisetti <rupeshdarimisetti@gmail.com>
2022-01-28 08:16:05 +0530

    Basic function declaration in js to turn off the laser setting

---
+++ laserFunction.js
@@ -0,0 +1,10 @@
+// Create a file called laserFunction.js in your code folder. Inside this file,
+//  create a JavaScript function called getLaserSetting.
+// This function should return a string object with the value of "OFF".
+function getLaserSetting(){
+    const settings ="OFF"
+    return settings
+}
+
+const currentSetting = getLaserSetting()
+console.log("The current laser setting is: " + currentSetting)
