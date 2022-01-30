4da38e1b949ee44691a84e4734d1fac1f146df4d
Rupesh-Darimisetti <rupeshdarimisetti@gmail.com>
2022-01-30 12:46:06 +0530

    output selected no of array elements

---
+++ getFirstAmountSorted.js
@@ -0,0 +1,16 @@
+function getFirstAmountSorted(inputArray, numberOfItems) {
+    // Step 1: sort inputArray alphabetically
+    let array = inputArray.sort();
+    // Step 2: create an array which contains the first N items of the sorted
+    //         array - the number of items is provided in the numberOfItems 
+    //         variable
+   let elements = array.slice(0, numberOfItems);
+    
+    // Step 3: return the new array you created at step 2
+    return elements;
+  }
+  
+  // The lines of code below test your function when you execute your code in
+  // the terminal - they are not required for your function to work
+  const newArray = getFirstAmountSorted(['cat', 'apple', 'bat'], 2);
+  console.log(newArray); // << should print out ['apple', 'bat']
